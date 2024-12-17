f3939a729f8e4ebff550a6fe795072ca0fb71077
consulforte <juliano.ti@consulforte.com.br>
2024-12-17 10:26:04 -0300

    Update config.sh

--- config/config.sh
+++ config/config.sh
@@ -1,28 +1,28 @@
 
 # Configurações de Rede e Credenciais
-SERVIDOR1_IP="192.168.1.10"  # IP do servidor 1
-SERVIDOR2_IP="192.168.1.11"  # IP do servidor 2
+SERVIDOR1_IP="192.168.1.55"  # IP do servidor 1
+SERVIDOR2_IP="192.168.1.56"  # IP do servidor 2
 
 # Domínio e Rede
-DOMINIO_VERIFICACAO="exemplo.com"
-REDE_LOCAL="192.168.1.0/24"
-REDE_REMOTA="10.0.0.0/16"
+DOMINIO_VERIFICACAO="consulforte.local"
+REDE_LOCAL="192.168.1.1/24"
+#REDE_REMOTA="10.0.0.0/16"
 
 # Senhas padrão
 GRAFANA_USER="admin"
-GRAFANA_PASS="admin"
+GRAFANA_PASS="S8c3ss0r"
 
 ZABBIX_USER="Admin"
-ZABBIX_PASS="zabbix"
+ZABBIX_PASS="S8c3ss0r"
 
 ELASTIC_USER="elastic"
-ELASTIC_PASS="elasticpassword"
+ELASTIC_PASS="S8c3ss0rC2nfort1"
 
 GLPI_USER="glpi"
-GLPI_PASS="glpi"
+GLPI_PASS="S8c3ss0r"
 
 NETBOX_USER="admin"
-NETBOX_PASS="admin"
+NETBOX_PASS="S8c3ss0r"
 
 WAZUH_USER="admin"
-WAZUH_PASS="admin"
+WAZUH_PASS="S8c3ss0r"
